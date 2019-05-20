e7192272e7663df33d8961ed406783cb7f070acc
TongDangTinh <tinhtongdang@gmail.com>
2019-05-20 23:10:52 +0700

    continue to create modeling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  has_many :product_orders, dependent: :destroy
+  has_many :rates
+  has_many :comments
+  has_many :categores, throught: :product_categories
+  has_many :product_categories
+  has_many :price, presence: true	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many  :addresses, dependent: :destroy
+  validates :name, presences: true
+  validates :phone, presences: true
+  validates :email, presences: true 	
+  validates :password, presences: true
 end
