c18aed102551850d76f50c05a49fa7bbc510be28
TongDangTinh <tinhtongdang@gmail.com>
2019-05-20 23:10:52 +0700

    continue to create modeling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :products, through: :orders
+  has_many  :addresses, dependent: :destroy
+  validates :name, presences: true
+  validates :phone, presences: true
+  validates :email, presences: true 	
+  validates :password, presences: true
 end
