52b258492fac28e941678bbc56e248823918f012
TongDangTinh <tinhtongdang@gmail.com>
2019-05-21 15:02:02 +0700

    Static pages and normal sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
-  has_many  :addresses, dependent: :destroy
-  validates :name, presences: true
-  validates :phone, presences: true
-  validates :email, presences: true 	
-  validates :password, presences: true
+  before_save :downcase_email
+  has_many :addresses, dependent: :destroy
+  accepts_nested_attributes_for :addresses, update_only: true
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :phone, presence:true
 end
