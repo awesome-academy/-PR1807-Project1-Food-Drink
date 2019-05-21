e508741ddac31df8bb9b8c8b8973c8439d9374d6
TongDangTinh <tinhtongdang@gmail.com>
2019-05-21 15:02:02 +0700

    Static pages and normal sign up

--- db/migrate/20190523153021_create_users.rb
+++ db/migrate/20190523153021_create_users.rb
@@ -3,13 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :address
-      t.string :address2
-      t.string :city
-      t.string :state
-      t.string :country
-      t.string :zipcode
-      t.string :phone
-      t.string :language
+      t.integer :phone
+      t.integer :role
+      t.string :avatar
 
       t.timestamps
     end
